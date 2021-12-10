d1cfc6263161bd3e58b5bdf26eddd951ac04f8e8
jiwon <73864010+nerisa2123@users.noreply.github.com>
2021-12-10 11:46:24 +0900

    CLC Test

---
+++ src/main/java/com/example/clc/clc_server/ClcServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.clc.clc_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClcServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clc/clc_server/ClcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clc.clc_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
